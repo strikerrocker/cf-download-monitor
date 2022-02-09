ca03f004f468bb23417624c79dd20083ca51bf88
StrikerRocker <strikerrockerplays@gmail.com>
2022-02-09 21:55:23 +0530

    Increase time b/w requests for saving downloads

--- scripts/save_tracked_projects_downloads.sh
+++ scripts/save_tracked_projects_downloads.sh
@@ -4,5 +4,5 @@ curl -s "${BASE_URL}/tracked_projects" |
 jq -c .[] | 
 while read id; 
 do 
-    curl -s "${BASE_URL}/save_project_downloads" -d "projectID=$id"; echo; 
+    curl -s "${BASE_URL}/save_project_downloads" -d "projectID=$id" && echo && sleep 2; 
 done
